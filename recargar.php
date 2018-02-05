688fa6d05ca8b8b0f54ac270d4238edd74efa417
Yasmin Levin <yasmin@cvam.com.ar>
2018-02-05 12:49:59 -0300

    Agregué un flujo en recargas online.

--- recargar.php
+++ recargar.php
@@ -1,100 +1,61 @@
 <!-- Header -->
 <?php include '_partials/head.php'; ?>
-
 <body>
 
-  <div>
-    <?php include '_partials/header.php'; ?>
-    <?php include '_partials/nav.php'; ?>
-  </div>
-  <?php include '_partials/actions.php'; ?>
 
-  <main class="grey-text text-darken-2">
-    <div class="container">
-      <div class="row">
-        <div class="col s12">
+	<?php include '_partials/header.php'; ?>
+	<?php include '_partials/nav.php'; ?>
+	<?php include '_partials/actions_emptystate.php'; ?>
 
-          <div class="card-panel">
-            <div class="card-content">
-              <form>
-                <h3 class="card-title mg-10">Recargas online</h3>
-                <p class="mg-10">La manera más fácil de obtener crédito para tus servicios.</p>
-                <div class="row">
-                  <div class="col s12 offset-m1 m10 offset-l1 l10 offset-xl1 xl10">
-                    <div class="row stock-images mg-10">
+	<main>
+		<div class="container">
 
-                      <div class="col s6 m4">
-                        <input id="movistar" name="carrier" type="radio" /><label for="movistar">
-                        <img src="/img/rubros/recargas.svg">
-                        </label>
-                        <a href="recargar.php" class="step-title waves-effect waves-white blue-text">Recarga celular</a>
-                      </div>
+			<div class="row">
+				<div class="col s12 m10 offset-m1">
 
-                      <div class="col s6 m4"><input id="claro" name="carrier" type="radio" /><label for="claro">
-                          <img src="/img/rubros/prepagos.svg">
-                        </label>
-                        <a href="recargar.php" class="step-title waves-effect waves-white blue-text">Carga prepago</a>
-                      </div>
+					<div class="card emptystate">
+						<div class="card-content">
+							<span class="card-title">Recargas online</span>
+							<p>La manera más fácil de obtener crédito para tus servicios.</p>
+							<div class="row center-align mg-t-40 mg-b-0">
+								<div class="col s12 m4 l4 mg-b-20">
+									<p><a href="recargas_moviles.php" ><img src="/img/rubros/recargas.svg" class="max-150" alt=""></p></a>
+									<a href="recargas_moviles.php" class="waves-teal btn-flat recharge-btn"><strong>RECARGA CELULAR</strong></a>
+								</div>
+								<div class="col s12 m4 l4 mg-b-20">
+									<p><img src="/img/rubros/prepagos.svg" class="max-150" alt=""></p>
+									<a href="alta_tarjeta.php" class="waves-teal btn-flat"><strong>CARGA PREPAGOS</strong></a>
+								</div>
 
-                      <div class="col s6 m4"><input id="test2" name="carrier" type="radio" /><label for="test2">
-                          <img src="/img/rubros/transportes.svg">
-                        </label>
-                        <a href="recargar.php" class="step-title waves-effect waves-white blue-text">Carga transporte</a>
-                      </div>
+								<div class="col s12 m4 l4 mg-b-20">
+									<p><img src="/img/rubros/transportes.svg" class="max-150" alt=""></p>
+									<p class="waves-teal btn-flat"><strong>CARGA TRANSPORTE</strong></p>
+							</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
-                    </div><!--/row stock-images-->
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
+			</div>
+
+		</div>
+	</main>
 
 
-  <!-- Javascript -->
-  <?php include '_partials/footer.php'; ?>
 
-  <script>
-    function anyThing() {
-      setTimeout(function () {
-        $('.stepper').nextStep();
-      }, 1500);
-    }
-    $(function () {
-      $(".stepper").activateStepper();
+<?php include '_partials/footer.php'; ?>
 
-    });
+<script>
+	$(document).ready(function(){
+		// //Inicializa tap-target
+		// setTimeout(function(){
+		// 	$('.tap-target').tapTarget('open');
+		// },300);
 
-    $(document).ready(function () {
-      //Esconder el carrito mobile del nav
-      $('.cantidad').hide();
-
-      $('.jp-card-name').hide();
-
-      $('.jp-card-front .jp-card-logo.jp-card-visa').text("");
-      $('.jp-card-front .jp-card-logo.jp-card-mastercard').text("");
-      $('.jp-card-front .jp-card-logo.jp-card-maestro').text("");
-      $('.jp-card-front .jp-card-logo.jp-card-amex').text("");
-    });
-
-    $(".date-card").keyup(function () {
-      if (this.value.length == this.maxLength) {
-        var $next = $('.date-card2').focus();
-        // console.log($next);
-        // if ($next.length){
-        //   $(this).next('.date-card').focus();
-        // }
-        // else {
-        //   $(this).blur();
-        // }
-      }
-    });
-  </script>
-
+		//Esconder carrito del nav
+		$('.cantidad').hide();
+	})
+</script>
 
 </body>
-
 </html>
